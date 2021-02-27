aa3bb335309c8d08a21a5ce4de7f5311a5ed3b43
yuziroppe <syani4732@gmail.com>
2021-02-27 19:32:54 +0900

    ホーム画面暫定作成

--- src/components/theme.tsx
+++ src/components/theme.tsx
@@ -4,7 +4,56 @@ import { red } from '@material-ui/core/colors';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#ffd180',
+      // 濃紫
+      main: '#800080',
+    },
+    daitoku: {
+      // 濃紫
+      main: '#800080',
+    },
+    syotoku: {
+      // 薄紫
+      main: '#CC99FF',
+    },
+    daizin: {
+      // 濃青
+      main: '#333399',
+    },
+    syozin: {
+      // 薄青
+      main: '#3366FF',
+    },
+    dairei: {
+      // 大礼
+      main: '#FF0000',
+    },
+    syorei: {
+      // 小礼
+      main: '#FF6600',
+    },
+    daishin: {
+      // 大信
+      main: '#FFFF00',
+    },
+    syoshin: {
+      // 小信
+      main: '#FFFF99',
+    },
+    daigi: {
+      // 大義
+      main: '#FFFFFF',
+    },
+    syogi: {
+      // 小義
+      main: '#DDDDDD',
+    },
+    daichi: {
+      // 大智
+      main: '#000000',
+    },
+    syochi: {
+      // 小智
+      main: '#808080',
     },
     secondary: {
       main: '#d32f2f',
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,70 @@
 import React from 'react';
 import Layout from '../components/layout';
-import CategoryCard from '../components/categoryCard';
-import { Card, CardMedia, Grid, makeStyles } from '@material-ui/core';
+import {
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  createStyles,
+  Grid,
+  LinearProgress,
+  makeStyles,
+  Typography,
+} from '@material-ui/core';
 import { selectCategories } from '../database';
 import { CategoryDocument } from '../database/model';
 import { Container } from '@material-ui/core';
+import { Theme } from '@material-ui/core/styles/createMuiTheme';
 
 interface AppProps {
   categories: CategoryDocument[];
 }
 
-const useStyles = makeStyles({
-  container: {
-    marginTop: '14px',
-  },
-});
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    easy: {
+      background: theme.palette.syochi.main,
+      color: 'white',
+      '&:hover': {
+        backgroundColor: theme.palette.daichi.main,
+      },
+      titleLink: {
+        color: 'white',
+        background: theme.palette.syochi,
+        textDecoration: 'none',
+      },
+    },
+    normal: {
+      background: theme.palette.syorei.main,
+      color: `white`,
+      '&:hover': {
+        backgroundColor: theme.palette.dairei.main,
+      },
+      titleLink: {
+        color: `white`,
+        textDecoration: 'none',
+      },
+    },
+    hard: {
+      background: theme.palette.syotoku.main,
+      color: 'white',
+      '&:hover': {
+        backgroundColor: theme.palette.daitoku.main,
+      },
+      titleLink: {
+        color: 'white',
+        background: theme.palette.syotoku,
+        textDecoration: 'none',
+      },
+    },
+    buttonCard: {
+      margin: theme.spacing(4),
+    },
+    levelRest: {
+      margin: theme.spacing(4),
+    },
+  }),
+);
 
 const Index = (props: AppProps) => {
   const classes = useStyles();
@@ -30,11 +80,102 @@ const Index = (props: AppProps) => {
           title="Contemplative Reptile"
         />
       </Card>
-      <Container maxWidth="lg" className={classes.container}>
+      <Container maxWidth="lg">
+        {/* レート表示 */}
         <Grid container>
-          {props.categories.map((category: CategoryDocument, index: number) => {
-            return <CategoryCard key={index}>{category}</CategoryCard>;
-          })}
+          <Grid item xs={12} md={12} lg={12} className={classes.buttonCard}>
+            <Card>
+              <CardContent className={classes.buttonCard}>
+                <Typography variant="h5" component="h5">
+                  現在の冠位:
+                </Typography>
+                <Typography
+                  gutterBottom
+                  variant="h2"
+                  component="h2"
+                  color="primary"
+                  align="right"
+                >
+                  小徳
+                </Typography>
+                <LinearProgress variant="determinate" value={10} />
+              </CardContent>
+              <CardContent className={classes.levelRest}>
+                <Typography
+                  variant="h6"
+                  component="h6"
+                  align="right"
+                  color="primary"
+                >
+                  大徳まで 877 / 1200 pts
+                </Typography>
+              </CardContent>
+            </Card>
+            <Grid container alignItems="center" justify="center"></Grid>
+          </Grid>
+        </Grid>
+        {/* 級 */}
+        <Grid container>
+          <Grid item xs={12} md={6} lg={4}>
+            <Card className={classes.buttonCard}>
+              <Grid container alignItems="center" justify="center">
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  width="100%"
+                  image="/static/imoko.svg"
+                  title="Contemplative Reptile"
+                />
+                <Button
+                  variant="contained"
+                  className={classes.easy}
+                  fullWidth={true}
+                >
+                  初級
+                </Button>
+              </Grid>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <Card className={classes.buttonCard}>
+              <Grid container alignItems="center" justify="center">
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  width="100%"
+                  image="/static/umako.svg"
+                  title="Contemplative Reptile"
+                />
+                <Button
+                  variant="contained"
+                  className={classes.normal}
+                  fullWidth={true}
+                >
+                  中級
+                </Button>
+              </Grid>
+            </Card>
+          </Grid>
+          <Grid item xs={12} md={6} lg={4}>
+            <Card className={classes.buttonCard}>
+              <Grid container alignItems="center" justify="center">
+                <CardMedia
+                  component="img"
+                  alt="Contemplative Reptile"
+                  width="100%"
+                  image="/static/taishi.svg"
+                  title="Contemplative Reptile"
+                />
+                <Button
+                  variant="contained"
+                  className={classes.hard}
+                  fullWidth={true}
+                >
+                  上級
+                </Button>
+              </Grid>
+            </Card>
+          </Grid>
         </Grid>
       </Container>
     </Layout>
@@ -43,6 +184,9 @@ const Index = (props: AppProps) => {
 
 Index.getInitialProps = async ({ req }) => {
   const categoryDocuments = await selectCategories();
+  /* {props.categories.map((category: CategoryDocument, index: number) => {
+    return <CategoryCard key={index}>{category}</CategoryCard>;
+  })} */
 
   // ルームの全てのカテゴリを取得
   const cats = await categoryDocuments.get();
