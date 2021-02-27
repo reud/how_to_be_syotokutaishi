d9315dd97b2c58d0a034d6e6cd0cac29a3c2f466
Piece123 <taka.rock.i4@gmail.com>
2021-02-27 12:49:31 +0900

    firebaseのkeyを追加

--- src/plugins/firebase.ts
+++ src/plugins/firebase.ts
@@ -3,14 +3,13 @@ import firebase from 'firebase';
 if (!firebase.apps.length) {
   // Please add your own API setting
   firebase.initializeApp({
-    apiKey: '####',
-    authDomain: '####',
-    databaseURL: '####',
-    projectId: '####',
-    storageBucket: '####',
-    messagingSenderId: '####',
-    appId: '####',
-    measurementId: '####',
+    apiKey: "AIzaSyDQIjEz93LVLIS5sAenM5ytp2k6vRHn6mw",
+    authDomain: "how-to-be-syotokutaishi.firebaseapp.com",
+    projectId: "how-to-be-syotokutaishi",
+    storageBucket: "how-to-be-syotokutaishi.appspot.com",
+    messagingSenderId: "1088012081831",
+    appId: "1:1088012081831:web:58fb841ac070827a475982",
+    measurementId: "G-D351GSSKFJ"
   });
 }
 
