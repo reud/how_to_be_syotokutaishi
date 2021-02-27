2a5afc2dd28af01ef43627106845cb79d303c61f
Piece123 <taka.rock.i4@gmail.com>
2021-02-28 02:22:27 +0900

    ロゴ周りを修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,7 +19,7 @@ export default function MyApp(props: AppProps) {
     <React.Fragment>
       <Head>
         <title>気軽にオンライン飲み会ができる！ | Cheers!</title>
-        <link rel="shortcut icon" href="/static/icon.png" />
+        <link rel="shortcut icon" href="/static/icon.svg" />
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
