5839def1a7f70540d8112d5a6b18bf1477b08ef8
reud <reiji.kobayashi.9n@stu.hosei.ac.jp>
2021-02-28 11:13:09 +0900

    変数名の修正

--- src/pages/answering/[...ids].tsx
+++ src/pages/answering/[...ids].tsx
@@ -79,7 +79,7 @@ const Answering = (props) => {
   const [currentUser, setCurrentUser] = useState<firebase.User>(null);
   const [quizzes, setQuizzes] = useState<Array<Problem>>([]);
   const [idx, setidx] = useState(0);
-  const [earnExp, setEarnExp] = useState(0);
+  const [solvedNum, setSolvedNum] = useState(0);
   const [result, setResult] = useState('解こう！');
 
   const router = useRouter();
@@ -119,7 +119,7 @@ const Answering = (props) => {
 
   const scoring = async (ans: boolean) => {
     if (quizzes[idx].collectAnswer === ans) {
-      setEarnExp(earnExp + 1);
+      setSolvedNum(solvedNum + 1);
       setResult('正解！');
     } else {
       setResult('不正解！');
@@ -132,7 +132,7 @@ const Answering = (props) => {
       const db = NewDatabase();
       const uid = (currentUser as firebase.User).uid;
       const userNowData = await db.fetchUserData(uid);
-      await db.updateUserData(uid, userNowData.exp, earnExp * 100 * ids.length);
+      await db.updateUserData(uid, userNowData.exp, solvedNum * 100 * ids.length);
       router.push('/');
     }
   };
